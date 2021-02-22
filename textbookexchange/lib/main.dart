e6fa66f86d131ccb726bcb84f7ff4b93febccf9b
Anya Shukla <68243595+ashukla9@users.noreply.github.com>
2021-02-22 14:43:01 -0800

    Adding vscode

--- textbookexchange/lib/main.dart
+++ textbookexchange/lib/main.dart
@@ -1,33 +1,27 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-//IMPORT STATEMENTS
 import 'package:flutter/material.dart';
-import 'package:mysql1/mysql1.dart';
 
 import 'screens/random_words.dart'; //importing the random words file from the screens folder
-import 'screens/home.dart'; //importing the home screen 
+import 'screens/home.dart'; //importing the home screen
 import 'package:textbookexchange/routes.dart'; //importing the routes
 
-
 void main() => runApp(MyApp()); //main method runs "MyApp"
-    //MyApp is "core" or "root" widget
+//MyApp is "core" or "root" widget
 
-class MyApp extends StatelessWidget { //immutable widget, stateless
+class MyApp extends StatelessWidget {
+  //immutable widget, stateless
   @override
   Widget build(BuildContext context) {
-
     // [NO LONGER USED] final wordPair = WordPair.random(); //final=const in js, can't directly reassign the variable. Can use const if value of var is always known.
 
     return MaterialApp(
       title: 'Welcome to Flutter',
-      theme: ThemeData(primaryColor: Colors.purple[50]), //change theme color and appbar color
+      theme: ThemeData(
+          primaryColor:
+              Colors.purple[50]), //change theme color and appbar color
 
       initialRoute: "/",
-      routes: routes, //routes is the Map we have in routes.dart [source: https://morioh.com/p/99b14cbf048e]
-
+      routes:
+          routes, //routes is the Map we have in routes.dart [source: https://morioh.com/p/99b14cbf048e]
     );
   }
 }
-
