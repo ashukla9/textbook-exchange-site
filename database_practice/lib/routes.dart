6a8580400d12b487ae08f069674f58b2f491202a
Elissa Martial <lmartial@hotmail.com>
2021-03-01 13:23:53 -0800

    reconciled the database app with the random words app we wrote

---
+++ database_practice/lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'screens/home.dart';
+import 'screens/random_words.dart';
+import 'screens/databasetest.dart';
+ 
+ //organizes all the routes (which 'urls' link to which page)
+
+ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{ //later add these routes to routes.dart
+   '/': (BuildContext context) => HomeScreen(),
+   '/randomWords': (BuildContext context) => RandomWords(), //adding a 'named' route that does nothing rn
+   '/databaseTesting': (BuildContext context) => MyHomePage(),
+ };               
--- database_practice/lib/screens/home.dart
+++ database_practice/lib/screens/home.dart
@@ -3,24 +3,35 @@
 //import statements: You basically always have to import flutter/material.dart
 import 'package:flutter/material.dart';
 
+import 'package:database_practice/routes.dart';
+
+import 'random_words.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Textable Home'),
-      ),
-      body: Container(
+        appBar: AppBar(
+      title: Text('Textbook Exchange Home'),
+    
+    ),
+    body: Container(
         child: Column(
           children: [
             Row( children: [
               ElevatedButton(
                 onPressed: () {
-                  Navigator.pushNamed(context, '/randomwords');
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => RandomWords()));
                 },
-                child: Text("Random Words App")
-              )
-            
+                child: Text("Random Words")
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/databaseTesting');
+                },
+                child: Text("Database Test")
+              ),
+  
               ]
             )
             
@@ -30,6 +41,7 @@ class HomeScreen extends StatelessWidget {
         ),
       
       )
+    
     );
   }
 }
