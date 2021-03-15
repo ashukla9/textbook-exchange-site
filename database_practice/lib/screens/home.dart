9fe79c4687eea7773e5c982ad57999115b6e32d5
Anya Shukla <68243595+ashukla9@users.noreply.github.com>
2021-03-15 15:15:31 -0700

    Adding authentication properly

--- database_practice/lib/screens/home.dart
+++ database_practice/lib/screens/home.dart
@@ -8,73 +8,61 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Padding(
-            padding: EdgeInsets.only(left: 12),
-                child: Image.asset(
-                'assets/lakesidelogo.jpg', //replace this with either our own logo or smth
-              ),
-            ),
-      title: Text('Textbook Exchange Home'),
-      
-    ),
-
-    body: Container(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.only(left: 12),
+          child: Image.asset(
+            'assets/lakesidelogo.jpg', //replace this with either our own logo or smth
+          ),
+        ),
+        title: Text('Textbook Exchange Home'),
+      ),
+      body: Container(
         child: Column(
           children: [
             SizedBox(height: 20), //adding space between widgets
-            
+
             Text(
               'LS Textbook Xchange',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 50, //how to specify?
-                ),
               ),
+            ),
 
             SizedBox(height: 20), //adding space between widgets
 
-            Wrap( children: [
+            Wrap(children: [
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/sellBooks');
-                },
-                child: Text("Sell Books")
-              ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sellBooks');
+                  },
+                  child: Text("Sell Books")),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/buyBooks');
-                },
-                child: Text("Buy Books")
-              ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/buyBooks');
+                  },
+                  child: Text("Buy Books")),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/cart');
-                },
-                child: Text("Cart")
-              ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/cart');
+                  },
+                  child: Text("Cart")),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/randomWords');
-                },
-                child: Text("Random Words")
-              ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/randomWords');
+                  },
+                  child: Text("Random Words")),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/databaseTesting');
-                },
-                child: Text("Database Test")
-              ),
-              
-              ]
-            ),
-            
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/databaseTesting');
+                  },
+                  child: Text("Database Test")),
+            ]),
           ],
         ),
-      
       ),
-
       endDrawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
@@ -97,16 +85,19 @@ class HomeScreen extends StatelessWidget {
             ),
             ListTile(
               title: Text('Sell Books'),
-              onTap: () { Navigator.pushNamed(context, '/buyBooks'); },
+              onTap: () {
+                Navigator.pushNamed(context, '/buyBooks');
+              },
             ),
             ListTile(
               title: Text('Profile'),
-              onTap: () { Navigator.pushNamed(context, '/profile'); },
+              onTap: () {
+                Navigator.pushNamed(context, '/profile');
+              },
             ),
           ],
         ),
       ),
-    
     );
   }
 }
